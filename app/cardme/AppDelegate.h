fc0b2ae41b323eaa662f3b4d424f2e1fbb9116e3
anteneh <am4970@nyu.edu>
2016-04-19 16:42:05 -0400

    added all files

---
+++ app/cardme/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cardme
+//
+//  Created by Anteneh Moges on 4/14/16.
+//  Copyright © 2016 nyu.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
